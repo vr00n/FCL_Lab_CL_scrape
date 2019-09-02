8c1acbfdf0f93d1ccb235e1cf321fa061f2b6ea4
Varun Adibhatla <vr00n@users.noreply.github.com>
2019-09-02 08:28:07 -0400

    Update CL-html-scraper.py

--- CL-html-scraper.py
+++ CL-html-scraper.py
@@ -1,20 +1,21 @@
-# Run in a Jupyter Notebook session
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[]:
-
-
 from bs4 import BeautifulSoup
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 import requests
-fileList = get_ipython().getoutput(u'ls *.html')
+import csv
+import os
+import sys
+## REPLACE DIRECTORY with PATH
+fileList = os.listdir('DIRECTORY')
 cl_place = []
 cl_coords = []
 cl_title = []
-
-# In[ ]:
-
+cl_url = []
 for filename in fileList:
+    #print filename
+## REPLACE DIRECTORY with PATH
+    filename = 'DIRECTORY'+filename
     soup = BeautifulSoup(open(filename), 'html.parser')
     html = list(soup.children)[2]
     head = list(html.children)[1]
@@ -23,11 +24,17 @@ for filename in fileList:
             column = i['name']
         except KeyError:
             column = i['property']
-        if column == "geo.placename":
+        if column == 'geo.placename':
             cl_place = i['content']
-        if column == "geo.position":
+        if column == 'geo.position':
             cl_coords = i['content']
-        if column == "og:title":
+        if column == 'og:title':
             cl_title = i['content'].strip()
-    cl_content = soup.find("section", {"id":"postingbody"}).text
-    print "!~!",cl_place,"|",cl_title,"|",cl_coords,"|",cl_content
+        if column == 'og:url':
+            cl_url = i['content'] 
+            cl_community = cl_url.split("/")[2]
+            cl_oid = cl_url.split("/")[-1]
+    cl_content = soup.find('section', {"id":'postingbody'}).text
+    cl_content = cl_content.replace("\n"," ")
+
+    print '!~!',cl_oid,'!~~!',cl_place,'!~~!',cl_title,'!~~!',cl_coords,'!~~!',cl_content,'!~~!',cl_community
